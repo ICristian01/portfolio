48afc00b1cdb0288d1d30d4bbc31abc637989ccc
ICristian01 <c.ionescu019@gmail.com>
2025-01-11 14:30:39 +1000

    updated portfolio project links

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,13 +3,13 @@ export default [
     title: "YouthMobilityHub",
     imgUrl: "/assets/ymh.png",
     stack: ["React", "Node.js", "MongoDb", "Express"],
-    link: "https://github.com",
+    link: "https://github.com/ICristian01/YouthMobilityHub",
   },
   {
     title: "Personal Portfolio",
     imgUrl: "/assets/portfolio.png",
     stack: ["React", "Tailwind", "Vite"],
-    link: "https://github.com",
+    link: "https://github.com/ICristian01/portfolio",
   },
   {
     title: "Bookshop",
