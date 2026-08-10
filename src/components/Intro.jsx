45c81f468a5162a54e56b58b4a1d41b216b97fde
ICristian01 <103032220+ICristian01@users.noreply.github.com>
2026-08-10 15:18:46 +1200

    Update Intro.jsx
    updated intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -13,11 +13,7 @@ function Intro() {
         Software Engineer & Backend Developer
       </p>
       <p className="text-sm max-w-xl mb-6 font-bold">
-        I am a software engineer currently studying in Australia. From past experiences, I have grown to appreciate my
-        ability to adapt to new situations and people while also developing my
-        technical knowledge and ability to learn. My current focus is on
-        back-end development while taking on  projects with the aim of transitioning to a
-        full-stack developer.
+        I am a software engineer having recently attained a Diploma in Cybersecurity on the Gold Coast, Australia. Within the past year, I’ve expanded my work experience across multiple areas, including food production, direct sales, and customer relations, while also developing my technical knowledge and ability to learn. My current focus is on back-end development and taking advantage of opportunities to become a full-stack developer.
       </p>
     </div>
   );
